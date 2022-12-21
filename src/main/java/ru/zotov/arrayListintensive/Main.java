c4a80ae9fb78de94f05d568a0454e5a2c1316e02
John <konstruuktor@gmail.com>
2022-12-21 18:52:49 +0300

    - .idea

--- src/main/java/ru/zotov/arrayListintensive/Main.java
+++ src/main/java/ru/zotov/arrayListintensive/Main.java
@@ -7,34 +7,34 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayListIntensive<Integer> testlist = new ArrayListIntensive<>();
-           for (int i = 25; i >= 0; i--) {
-            testlist.add(i);
+//        ArrayListIntensive<Integer> testlist = new ArrayListIntensive<>();
+//           for (int i = 25; i >= 0; i--) {
+//            testlist.add(i);
         }
-        System.out.println(testlist);
-
-        int v = testlist.size();
-        System.out.println(v);
-
-        QuickSort<Integer> quickSort = new QuickSort<>();
-        Integer[] sorted = quickSort.sort(testlist);
-
-        ArrayListIntensive<Integer> sortedlist = new ArrayListIntensive<>(sorted);
-        System.out.println(sortedlist);
-
-        testlist.update(8, 777);
-        System.out.println(testlist);
-
-        testlist.delete(5);
-        System.out.println(testlist);
-
-        int y = testlist.get(9);
-        System.out.println(y);
-
-        testlist.clear();
-        System.out.println(testlist);
-
-    }
+//        System.out.println(testlist);
+//
+//        int v = testlist.size();
+//        System.out.println(v);
+//
+//        QuickSort<Integer> quickSort = new QuickSort<>();
+//        Integer[] sorted = quickSort.sort(testlist);
+//
+//        ArrayListIntensive<Integer> sortedlist = new ArrayListIntensive<>(sorted);
+//        System.out.println(sortedlist);
+//
+//        testlist.update(8, 777);
+//        System.out.println(testlist);
+//
+//        testlist.delete(5);
+//        System.out.println(testlist);
+//
+//        int y = testlist.get(9);
+//        System.out.println(y);
+//
+//        testlist.clear();
+//        System.out.println(testlist);
+//
+//    }
 
 
 }
